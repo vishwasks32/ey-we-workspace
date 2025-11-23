b91e8a72de3c8d1a358c96ec8de9aae87db5c2e3
vishwasks32 <vishwasks32@gmail.com>
2025-11-23 15:30:13 +0530

    Refactor add function to include an optional parameter and update greet function for improved flexibility

--- demo_functions.py
+++ demo_functions.py
@@ -3,10 +3,12 @@ def func_name():
     print(f"Value of v in func_name() level is : {v}")
     print("I am inside func_name()")
 
-def add(a,b):
+def add(a,b, c=10):
     print(f"Value of v in add() level is : {v}")
-    return a+b
+    return a+b+c
 
+def greet(username, prepend="Hello", append='Welcome to session'):
+    return prepend + ' '+ username + ' ' + append
 
 if __name__ == '__main__':
-    k = add(2,3)
+    print(greet('Vishwas',append='Welcome to python Session'))
