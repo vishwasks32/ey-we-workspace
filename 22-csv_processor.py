b4ccf63c4539779edd52225fa007c3ac74a08e62
vishwasks32 <vishwasks32@gmail.com>
2025-11-29 15:11:12 +0530

    Refactor file_loader to use csv.DictReader for improved data handling and clarity

--- 22-csv_processor.py
+++ 22-csv_processor.py
@@ -10,10 +10,12 @@ def file_loader(fname):
     try:
         rows = []
         with open(fname,"rt") as fh:
-            data_reader = csv.reader(fh)
+            data_reader = csv.DictReader(fh)
+            print(f"{data_reader.fieldnames}")
             for row in data_reader:
-                print(f"Name: {row[0]}")
-                rows.append((row[0],row[1]))
+                name = row['Name']
+                email = row['Email']
+                rows.append({"user_name": name,"email":email})
 
     except FileNotFoundError:
         print("The data file is not found")
@@ -23,8 +25,9 @@ def file_loader(fname):
 def setup():
     data_rows = file_loader(DATA_FILE)
     # print(f"The number of rows in data: {len(data_rows)}")
-    for k,v in data_rows:
-        print(f"Name: {k},Email: {v}")
+    print(data_rows)
+    # for k,v in data_rows:
+    #     print(f"Name: {k},Email: {v}")
 
 def cli_interface():
     pass
